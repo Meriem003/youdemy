68e9b1d948730837e93f81265ee7700afff93318
meryam <meryemsalhi590@gmail.com>
2025-01-13 16:24:51 +0100

    1ére version front end

---
+++ vue/user/admin/dashboard.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>courses</title>
+
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+
+   <link rel="stylesheet" href="../../../public/css/style.css">
+
+</head>
+<body>
+
+<header class="header">
+   <section class="flex">
+      <a href="home.html" class="logo">skillshare</a>
+      <div class="icons">
+         <div id="menu-btn" class="fas fa-bars"></div>
+         <div id="search-btn" class="fas fa-search"></div>
+         <div id="user-btn" class="fas fa-user"></div>
+         <div id="toggle-btn" class="fas fa-sun"></div>
+      </div>
+
+      <div class="profile">
+         <img src="../../../public/images/pic-1.jpg" class="image" alt="">
+         <h3 class="name">meryam salhi</h3>
+         <p class="role">admin</p>
+         <div class="flex-btn">
+            <a href="../pages/about.php" class="option-btn">déconnecté</a>
+         </div>
+      </div>
+
+   </section>
+
+</header>   
+
+<div class="side-bar">
+
+   <div id="close-btn">
+      <i class="fas fa-times"></i>
+   </div>
+
+   <div class="profile">
+      <img src="../../../public/images/pic-1.jpg" class="image" alt="">
+      <h3 class="name">meryam salhi</h3>
+      <p class="role">admin</p>
+      <a href="../pages/about.php" class="option-btn">déconnecté</a>
+   </div>
+
+   <nav class="navbar">
+      <a href="courses.html"><i class="fas fa-graduation-cap"></i><span>courses</span></a>
+      <a href="teachers.html"><i class="fas fa-chalkboard-user"></i><span>teachers</span></a>
+   </nav>
+</div>
+
+
+<script src="../../../public/js/script.js"></script>
+</body>
+</html>
